3447496a079a4b61d8db80d905e2b1f5dd37fb3c
omar01220616563 <omar8nawara@gmail.com>
2025-10-26 20:40:00 +0300

    Refactor code structure for improved readability and maintainability

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./core/layouts/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

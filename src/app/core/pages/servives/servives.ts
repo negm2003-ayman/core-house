ccaa4124586db6047b392ea627fcbcbb4a7fd19f
negm2003-ayman <elnemsnegm@gmail.com>
2025-10-26 19:39:27 +0300

    feat: add initial components and templates for footer, navbar, about, contact, home, portfolio, and servives

---
+++ src/app/core/pages/servives/servives.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servives',
+  imports: [],
+  templateUrl: './servives.html',
+  styleUrl: './servives.css',
+})
+export class Servives {
+
+}

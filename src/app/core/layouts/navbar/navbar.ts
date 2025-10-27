a008ce119dd5961acf26505310694677562d64bc
omar01220616563 <omar8nawara@gmail.com>
2025-10-27 14:02:00 +0300

    feat: enhance app layout with navbar, footer, and home components; add styling and transitions

--- src/app/core/layouts/navbar/navbar.ts
+++ src/app/core/layouts/navbar/navbar.ts
@@ -1,5 +1,5 @@
 import { Flowbite } from './../../services/Flowbite/flowbite';
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 
 @Component({
@@ -9,7 +9,19 @@ import { initFlowbite } from 'flowbite';
   styleUrl: './navbar.css',
 })
 export class Navbar {
-    constructor(private flowbite: Flowbite) {}
+  // navBar transtion
+  @ViewChild('nav') nav!: any;
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event):void {
+    if(window.scrollY > 10){
+      this.nav.nativeElement.classList.replace('py-3' , 'py-1')
+    }else {
+      this.nav.nativeElement.classList.replace('py-1' , 'py-3')
+    }
+  }
+
+  constructor(private flowbite: Flowbite) {}
+
 
   ngOnInit(): void {
     this.flowbite.loadFlowbite((Flowbite) => {
